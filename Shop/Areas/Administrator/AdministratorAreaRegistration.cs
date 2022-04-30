62e30f68c7e756f3751efec425a2e8d96d19e69c
Huỳnh <huynh.it.24@gmail.com>
2022-04-30 18:07:42 +0700

    thêm Areas MVC cho Admin

---
+++ Shop/Areas/Administrator/AdministratorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Shop.Areas.Administrator
+{
+    public class AdministratorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Administrator";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Administrator_default",
+                "Administrator/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
