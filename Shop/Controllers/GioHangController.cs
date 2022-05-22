f0bd8d9070169f54ca4fed24c54d1b9ee22bed29
Huỳnh <huynh.it.24@gmail.com>
2022-05-23 02:47:55 +0700

    VnPay research

--- Shop/Controllers/GioHangController.cs
+++ Shop/Controllers/GioHangController.cs
@@ -2,6 +2,7 @@
 using Shop.Mail;
 using Shop.Models;
 using Shop.MoMo;
+using Shop.VnPay.Others;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -383,8 +384,8 @@ namespace Shop.Controllers
             string param = Request.QueryString.ToString().Substring(0, Request.QueryString.ToString().IndexOf("signature") - 1);
             param = Server.UrlDecode(param);
             MoMoSecurity crypto = new MoMoSecurity();
-            //string secretkey = ConfigurationManager.AppSettings["serectkey"].ToString();
-            string secretkey = "gZ2H5gyDOrVLQ0mnVJjPCWQ4a2lenHLN";
+            string secretkey = ConfigurationManager.AppSettings["serectkey"].ToString();
+            //string secretkey = "gZ2H5gyDOrVLQ0mnVJjPCWQ4a2lenHLN";
             string signature = crypto.signSHA256(param, secretkey);
             if (signature != Request["signature"].ToString())
             {
@@ -408,7 +409,8 @@ namespace Shop.Controllers
             string param = Request.QueryString.ToString().Substring(0, Request.QueryString.ToString().IndexOf("signature") - 1);
             param = Server.UrlDecode(param);
             MoMoSecurity crypto = new MoMoSecurity();
-            string secretkey = ConfigurationManager.AppSettings["serectkey"].ToString();
+            //string secretkey = ConfigurationManager.AppSettings["serectkey"].ToString();
+            string secretkey = "gZ2H5gyDOrVLQ0mnVJjPCWQ4a2lenHLN";
             string signature = crypto.signSHA256(param, secretkey);
             if (signature != Request["signature"].ToString())
             {
@@ -474,5 +476,93 @@ namespace Shop.Controllers
 
             data.SubmitChanges();
         }
+
+        /*Thanh toán VNPAY && ZaloPay*/
+
+        //Thanh toán VNPAY
+        public ActionResult Payment()
+        {
+            DonHang dh = new DonHang();
+            AspNetUser kh = (AspNetUser)Session["TaiKhoan"];// ép session về kh để lấy thông tin
+            Laptop s = new Laptop();
+            List<GioHang> gh = Laygiohang();// lấy giỏ hàng
+
+            //string url = ConfigurationManager.AppSettings["Url"];
+            //string returnUrl = ConfigurationManager.AppSettings["ReturnUrl"];
+            //string tmnCode = ConfigurationManager.AppSettings["TmnCode"];
+            //string hashSecret = ConfigurationManager.AppSettings["HashSecret"];
+
+            string url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+            string returnUrl = "https://localhost:44381/GioHang/XacnhanDonhang";
+            string tmnCode = "RFRCD0FS";
+            string hashSecret = "CCSNJNJWPZAHKOAWKPZSSPGRHMETMIPP";
+
+            PayLib pay = new PayLib();
+
+            pay.AddRequestData("vnp_Version", "2.0.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.0.0
+            pay.AddRequestData("vnp_Command", "pay"); //Mã API sử dụng, mã cho giao dịch thanh toán là 'pay'
+            pay.AddRequestData("vnp_TmnCode", tmnCode); //Mã website của merchant trên hệ thống của VNPAY (khi đăng ký tài khoản sẽ có trong mail VNPAY gửi về)
+            pay.AddRequestData("vnp_Amount", gh.Sum(p => p.dThanhTien).ToString()); //số tiền cần thanh toán, công thức: số tiền * 100 - ví dụ 10.000 (mười nghìn đồng) --> 1000000
+            pay.AddRequestData("vnp_BankCode", ""); //Mã Ngân hàng thanh toán (tham khảo: https://sandbox.vnpayment.vn/apis/danh-sach-ngan-hang/), có thể để trống, người dùng có thể chọn trên cổng thanh toán VNPAY
+            pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss")); //ngày thanh toán theo định dạng yyyyMMddHHmmss
+            pay.AddRequestData("vnp_CurrCode", "VND"); //Đơn vị tiền tệ sử dụng thanh toán. Hiện tại chỉ hỗ trợ VND
+            pay.AddRequestData("vnp_IpAddr", Util.GetIpAddress()); //Địa chỉ IP của khách hàng thực hiện giao dịch
+            pay.AddRequestData("vnp_Locale", "vn"); //Ngôn ngữ giao diện hiển thị - Tiếng Việt (vn), Tiếng Anh (en)
+            pay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang"); //Thông tin mô tả nội dung thanh toán
+            pay.AddRequestData("vnp_OrderType", "other"); //topup: Nạp tiền điện thoại - billpayment: Thanh toán hóa đơn - fashion: Thời trang - other: Thanh toán trực tuyến
+            pay.AddRequestData("vnp_ReturnUrl", returnUrl); //URL thông báo kết quả giao dịch khi Khách hàng kết thúc thanh toán
+            pay.AddRequestData("vnp_TxnRef", DateTime.Now.Ticks.ToString()); //mã hóa đơn
+
+            string paymentUrl = pay.CreateRequestUrl(url, hashSecret);
+
+            return Redirect(paymentUrl);
+        }
+
+        //Xác thực thanh toán VNPAY
+        /*public ActionResult PaymentConfirm()
+        {
+            if (Request.QueryString.Count > 0)
+            {
+                string hashSecret = ConfigurationManager.AppSettings["HashSecret"]; //Chuỗi bí mật
+                var vnpayData = Request.QueryString;
+                PayLib pay = new PayLib();
+
+                //lấy toàn bộ dữ liệu được trả về
+                foreach (string s in vnpayData)
+                {
+                    if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
+                    {
+                        pay.AddResponseData(s, vnpayData[s]);
+                    }
+                }
+
+                long orderId = Convert.ToInt64(pay.GetResponseData("vnp_TxnRef")); //mã hóa đơn
+                long vnpayTranId = Convert.ToInt64(pay.GetResponseData("vnp_TransactionNo")); //mã giao dịch tại hệ thống VNPAY
+                string vnp_ResponseCode = pay.GetResponseData("vnp_ResponseCode"); //response code: 00 - thành công, khác 00 - xem thêm https://sandbox.vnpayment.vn/apis/docs/bang-ma-loi/
+                string vnp_SecureHash = Request.QueryString["vnp_SecureHash"]; //hash của dữ liệu trả về
+
+                bool checkSignature = pay.ValidateSignature(vnp_SecureHash, hashSecret); //check chữ ký đúng hay không?
+
+                if (checkSignature)
+                {
+                    if (vnp_ResponseCode == "00")
+                    {
+                        //Thanh toán thành công
+                        ViewBag.Message = "Thanh toán thành công hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId;
+                    }
+                    else
+                    {
+                        //Thanh toán không thành công. Mã lỗi: vnp_ResponseCode
+                        ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId + " | Mã lỗi: " + vnp_ResponseCode;
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Có lỗi xảy ra trong quá trình xử lý";
+                }
+            }
+
+            return View();
+        }*/
     }
 }
---
+++ Shop/VnPay/Others/PayCompare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Shop.VnPay.Others
+{
+    public class PayCompare : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == y) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+            var Compare = CompareInfo.GetCompareInfo("en-US");
+            return Compare.Compare(x, y, CompareOptions.Ordinal);
+        }
+    }
+}
